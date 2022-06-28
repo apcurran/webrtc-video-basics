5f9b5f22b5ecf733f96a5629157ed32d2a911410
Alex Curran <alexcurran.va@gmail.com>
2022-06-28 16:42:17 -0500

    Basic server setup

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 "use strict";
+
+const express = require("express");
+const socket = require("socket.io");
+const http = require("http");
+
+const app = express();
+const server = http.createServer(app);
+const io = socket(server);
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
